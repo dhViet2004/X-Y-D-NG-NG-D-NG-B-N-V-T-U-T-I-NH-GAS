90ef19cbd2530f81fe4d0982012d28aa9a0b9be2
dhViet2004 <168359369+dhViet2004@users.noreply.github.com>
2024-10-02 23:55:21 +0700

    entity

--- App_QuanLyBanVeTauTaiNhaGa_GoVap/src/Main/Main.java
+++ App_QuanLyBanVeTauTaiNhaGa_GoVap/src/Main/Main.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        
+
         Entity_NhanVien HoangViet = new Entity_NhanVien("NV01","Hoàng Việt","0364601530","Nghỉ việc","123456","Bến tre", LocalDate.parse("2020-12-31"),"Quản lý");
         System.out.println(HoangViet);
     }
