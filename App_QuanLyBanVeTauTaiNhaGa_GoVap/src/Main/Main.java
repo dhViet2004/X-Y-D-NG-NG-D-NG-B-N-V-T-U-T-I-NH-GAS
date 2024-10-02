c4872100c5e28278e60661a7b765a024d4d90091
dhViet2004 <168359369+dhViet2004@users.noreply.github.com>
2024-10-02 23:06:49 +0700

    entity,dao,gui

---
+++ App_QuanLyBanVeTauTaiNhaGa_GoVap/src/Main/Main.java
@@ -0,0 +1,10 @@
+package Main;
+
+import Entity.User;
+
+public class Main {
+    public static void main(String[] args) {
+        User HoangViet = new User(12,"HoangViet");
+        System.out.println(HoangViet);
+    }
+}
