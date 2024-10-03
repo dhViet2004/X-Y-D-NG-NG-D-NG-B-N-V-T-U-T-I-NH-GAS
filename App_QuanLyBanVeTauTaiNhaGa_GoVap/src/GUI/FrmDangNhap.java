ee1d176918bf4c63347d2087ee63ee19b0a8258c
dhViet2004 <168359369+dhViet2004@users.noreply.github.com>
2024-10-04 00:35:16 +0700

    GUI Dang nhap

--- App_QuanLyBanVeTauTaiNhaGa_GoVap/src/GUI/FrmDangNhap.java
+++ App_QuanLyBanVeTauTaiNhaGa_GoVap/src/GUI/FrmDangNhap.java
@@ -1,36 +1,29 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class FrmDangNhap {
-    private JPanel panel1;
+public class FrmDangNhap extends JDialog {
+    private JPanel contentPane;
+    private JLabel banner;
+    private JPanel jPanel_Submit;
+    private JButton dangNhapButton;
+    private JPanel Jpanel_Input;
     private JTextField textField1;
     private JTextField textField2;
-    private JButton button1;
 
-    // Constructor to initialize the frame
-    public FrmDangNhap() {
-        // Create the JFrame
-        JFrame frame = new JFrame("Đăng nhập");
-
-        // Set size for the frame
-        frame.setSize(500, 500);
-        // Add the panel to the frame
-        frame.setContentPane(panel1);
-
-        // Set default close operation
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Make sure the frame is not resizable
-        frame.setResizable(false);
-        // Make the frame visible
-        frame.setVisible(true);
-        // Center the frame on the screen
-        frame.setLocationRelativeTo(null);
+    public FrmDangNhap (JFrame parent){
+        super(parent);
+        setContentPane(contentPane);
+        setTitle("Đăng nhập");
+        setMinimumSize(new Dimension(850, 462));
+        setModal(true);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(contentPane);
+        setVisible(true);
     }
 
     public static void main(String[] args) {
-        // Run the login form
-        new FrmDangNhap();
+        FrmDangNhap frmDangNhap = new FrmDangNhap(null);
     }
 }
